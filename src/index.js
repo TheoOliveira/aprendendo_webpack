38e7a37b701437c6fdc90490695445dcaf6f4291
TheoOliveira <akai.hen@gmail.com>
2020-01-17 09:16:59 -0300

    estudo inicial usando o getting started do site oficial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+function component(){
+	const element = document.createElement('div');
+
+
+	// adicionar no div element
+	element.innerHTML = _.join(['Hello', 'webpack'], '');
+
+	return element;
+
+
+}
+
+
+document.body.appendChild(component());
